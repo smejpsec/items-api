956cedc222cd10426a31d35709d44a189a6e93aa
SME セキュア <contact@smejpsec.com>
2023-05-14 00:47:01 +0900

    コンパイラが自動的に借用する値の借用処理を削除

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ struct ItemQuery {
 #[get("/api/items")]
 async fn get_item(query: web::Query<ItemQuery>) -> impl Responder {
     // TODO: 在庫確認処理
-    let name = (&query.name).to_string();
+    let name = (query.name).to_string();
     let amount = 30;
 
     let item = Item { name, amount };
